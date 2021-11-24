0b7a3c8e57fcda9a561efa5b32918c8c6ddcbf5a
victorienpichereau <victorien.pichereau@orange.fr>
2021-11-24 08:59:32 +0100

    fonctionloss

---
+++ MIL.py
@@ -0,0 +1,63 @@
+import tensorflow as tf 
+
+
+def custom_objective(y_true, y_pred):
+
+    y_true = tf.reshape(y_true, [-1]) #permet d'applatir les données 
+    y_pred = tf.reshape(y_pred, [-1]) #permet d'applatir les données
+    n_seg = 32
+    nvid = 60 
+    n_exp = int(nvid / 2)
+    
+    min_scores_list = []
+    max_scores_list = []
+    temporal_constrains_list = []
+    sparsity_constrains_list = []
+    list_difference = []
+    l1_list = []
+    l2_list = []
+    l3_list = []
+    l4_list = []
+    
+    mu1 = 0.00004
+    mu2 = 0.00004
+
+    for i in range(0, n_exp, 1):
+        
+        video_predictions = y_pred[i*n_seg:(i+1)*n_seg]
+        max_scores_list.append(max(video_predictions))
+        min_scores_list.append(min(video_predictions))
+        for j in range(len(video_predictions)-1) : 
+            list_difference.append = video_predictions[j] - video_predictions[j+1]
+        temporal_constrains_list.append(sum(tf.pow(list_difference, 2)))
+        
+    for j in range(n_exp, 2*n_exp, 1):
+        
+        video_predictions = y_pred[j*n_seg:(j+1)*n_seg]
+        max_scores_list.append(max(video_predictions))
+        sparsity_constrains_list.append(sum(video_predictions))
+        
+    score_positive_groupe_desc = y_pred[0:n_exp].sort(reverse = True) #probleme sort ne sapplique pas à un tenseur
+    max_scores = tf.stack(max_scores_list)
+    min_scores = tf.stack(min_scores_list)
+    temporal_constrains = tf.stack(temporal_constrains_list)
+    sparsity_constrains = tf.stack(sparsity_constrains_list)
+
+    for ii in range(0, n_exp, 1):
+        
+    #Calcul de la liste des l1 :
+        l1_list.append(0,1 - max_scores[ii] + max_scores[n_exp+ii])
+    #Calcul de la liste des l2:
+        l2_list.append(0,1 - max_scores[ii] + min_scores[ii])        
+    #Calcul de la liste des l3:     
+        l3_list.append(0,1 - score_positive_groupe_desc[1] + min_scores[n_exp+ii])
+    #Calcul de la liste des l4:     
+        l4_list.append(0,1 - score_positive_groupe_desc[2] + min_scores[n_exp+ii])       
+        
+        
+
+    return max(l1_list) + max(l2_list) + max(l3_list) + max(l4_list) + mu1*sum(temporal_constrains) + mu2*sum(sparsity_constrains)
+    
+    
+
+    
